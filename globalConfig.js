a86073fc965822e2bfc6c12c278df4592066d182
lihuizhan <49526923+lihuizhan@users.noreply.github.com>
2023-11-05 22:05:36 +0800

    Update globalConfig.js

--- globalConfig.js
+++ globalConfig.js
@@ -1,3 +1,7 @@
+export const XHS_HOSTNAME = 'www.xiaohongshu.com'
+
+export const OPTIONS_CONFIG_KEY = '__options_config__'
+
 // inject 发消息 到 content script
 export const INJECT_TO_CONTENT_SCRIPT = 'inject_to_content_script'
 
@@ -9,3 +13,9 @@ export const CONTENT_TO_BACKGROUND = 'content_to_background'
 
 // content script 发列表消息 到 background
 export const CONTENT_TO_BACKGROUND__LIST = 'content_to_background__list'
+
+export const BACKGROUND_TO_OFFSCREEN = 'background_to_offscreen'
+
+// export const BACKGROUND_TO_OFFSCREEN__BATCH = 'background_to_offscreen__batch'
+
+export const OFFSCREEN_TO_BACKGROUND = 'offscreen_to_background'
